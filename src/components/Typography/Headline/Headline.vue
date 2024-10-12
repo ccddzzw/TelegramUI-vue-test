cb9ad0eb09d96988e069b0d07c15cc0646ae3855
ccddzzw <ccddzzw@gmail.com>
2024-10-12 10:52:13 +0800

    Update Headline

--- src/components/Typography/Headline/Headline.vue
+++ src/components/Typography/Headline/Headline.vue
@@ -1,24 +1,21 @@
 <template>
     <Typography
-        :class="classes"
         v-bind="$props"
+        :class="[
+          styles.wrapper,
+        ]"
     >
         <slot></slot>
     </Typography>
 </template>
   
 <script lang="ts" setup>
-  import { computed } from 'vue';
-  import { classNames } from '../../../helpers/classNames';
   import styles from './Headline.module.css';
-  import { TypographyProps } from '../Typography/Typography';
-  import Typography from '../Typography/Typography.vue';
+  import { TypographyProps, Typography } from '../Typography';
   
   interface HeadlineProps extends TypographyProps {}
   
-  const props = withDefaults(defineProps<HeadlineProps>(), {
+  withDefaults(defineProps<HeadlineProps>(), {
     component: 'h5',
   });
-  
-  const classes = computed(() => classNames(styles.wrapper, props.class));
 </script>
